11cf465cecbbf2537807901393035ece1f7f28e6
eventhorizonsky <eventhorizonsky@gmail.com>
2024-10-26 21:16:59 +0800

    端口修改

--- api.py
+++ api.py
@@ -28,4 +28,4 @@ def download_url():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5051)
